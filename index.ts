92bd6e608da1463c6b4f282ddaad8399d157d274
hslam <791874158@qq.com>
2023-06-16 16:12:40 +0800

    creates a manually managing EC2 node group
    Signed-off-by: hslam <791874158@qq.com>

--- index.ts
+++ index.ts
@@ -3,21 +3,59 @@
 
 import * as awsx from "@pulumi/awsx";
 import * as eks from "@pulumi/eks";
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as iam from "./iam";
 
-// Create a VPC for our cluster.
-const vpc = new awsx.ec2.Vpc("vpc", { numberOfAvailabilityZones: 3 });
+const config = new pulumi.Config();
 
-// Create an EKS cluster with some configurations.
-const cluster = new eks.Cluster("cluster", {
-    vpcId: vpc.id,
-    subnetIds: vpc.publicSubnetIds,
+export const env = config.require("env");
+export const region = config.require("region");
+export const prefix = `${env}-${region}-tidb`;
+export const cidrBlock = config.require("cidr-block");
+const numberOfAvailabilityZones = config.getNumber("availability-zones") || 3;
+const k8sVersion = config.require("k8s-version");
+
+// Allocate a new VPC with custom settings, and a public & private subnet per AZ.
+const vpc = new awsx.ec2.Vpc(`${prefix}-vpc`, {
+    numberOfAvailabilityZones: numberOfAvailabilityZones,
+    cidrBlock: cidrBlock,
+    subnets: [{type: "public"}, {type: "private"}],
+});
+
+// Export VPC ID and Subnets.
+export const vpcId = vpc.id;
+export const allVpcSubnets = pulumi.all([vpc.privateSubnetIds, vpc.publicSubnetIds])
+    .apply(([privateSubnetIds, publicSubnetIds]) => privateSubnetIds.concat(publicSubnetIds));
+
+// Create a IAM Roles and matching InstanceProfile to use with the nodegroups.
+const managedASGRole = iam.createRole(`${prefix}-managed-nodegroup-role`);
+const instanceProfile = new aws.iam.InstanceProfile(`${prefix}-instance-profile`, {role: managedASGRole});
+
+// Create an EKS cluster without node group attached
+const cluster = new eks.Cluster(`${prefix}-cluster`, {
+    version: k8sVersion,
+    name: prefix,
+    vpcId: vpcId,
+    subnetIds: allVpcSubnets,
+    nodeAssociatePublicIpAddress: false,
+    deployDashboard: false,
+    createOidcProvider: true,
+    useDefaultVpcCni: true,
+    endpointPrivateAccess: true,
+    endpointPublicAccess: true,
+    skipDefaultNodeGroup: true,
+    instanceRoles: [managedASGRole],
+});
+
+// Create an AWS node group using a cluster as input.
+const nodeGroup = cluster.createNodeGroup("${prefix}-node-group", {
     instanceType: "t2.medium",
+    maxSize: 2,
     desiredCapacity: 2,
     minSize: 1,
-    maxSize: 2,
-    storageClasses: "gp2",
-    deployDashboard: false,
-    version: "1.24",
+    labels: { "ondemand": "true" },
+    instanceProfile: instanceProfile,
 });
 
 // Export the cluster's kubeconfig.
