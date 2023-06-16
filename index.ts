bdf02b9420876d73c3a8232f762bb3016cbf6984
hslam <791874158@qq.com>
2023-06-16 15:42:48 +0800

    create an EKS cluster with some configurations
    Signed-off-by: hslam <791874158@qq.com>

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
 // Copyright (c) 2023 Meng Huang (mhboy@outlook.com)
 // This package is licensed under a MIT license that can be found in the LICENSE file.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as awsx from "@pulumi/awsx";
 import * as eks from "@pulumi/eks";
 
-// Create an EKS cluster with the default configuration.
-const cluster = new eks.Cluster("cluster", {});
+// Create a VPC for our cluster.
+const vpc = new awsx.ec2.Vpc("vpc", { numberOfAvailabilityZones: 3 });
+
+// Create an EKS cluster with some configurations.
+const cluster = new eks.Cluster("cluster", {
+    vpcId: vpc.id,
+    subnetIds: vpc.publicSubnetIds,
+    instanceType: "t2.medium",
+    desiredCapacity: 2,
+    minSize: 1,
+    maxSize: 2,
+    storageClasses: "gp2",
+    deployDashboard: false,
+    version: "1.24",
+});
 
 // Export the cluster's kubeconfig.
 export const kubeconfig = cluster.kubeconfig;
